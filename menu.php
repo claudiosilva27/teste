953d68f57af8577d5928b73d0af1b7a60981e2f6
claudiosilva27 <ccs.gpsia.2019@gmail.com>
2021-03-21 17:11:01 +0000

    todos

---
+++ menu.php
@@ -0,0 +1,90 @@
+<html>
+<head><link rel="stylesheet" href="menu.css">
+    
+</head>
+    
+    
+    <img src="logo.png" width=200px height="200px">
+    
+    
+    
+    
+    
+<div class="navbar">
+    <div class="dropdown">
+    <button class="dropbtn">RC1
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="http://rcexerciciosgpsia.epizy.com/phprc/exercicio1.php">ex1</a>
+      <a href="http://rcexerciciosgpsia.epizy.com/phprc/exercicio2.php">ex2</a>
+      <a href="http://rcexerciciosgpsia.epizy.com/phprc/exercicio3.php">ex3</a>
+      <a href="http://rcexerciciosgpsia.epizy.com/phprc/exercicio4.php">ex4</a>
+        
+        
+    </div>
+  </div>
+    
+    <div class="dropdown">
+    <button class="dropbtn">RC2
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="http://rcexerciciosgpsia.epizy.com/phprc2/exercicio1.php">ex1</a>
+      <a href="http://rcexerciciosgpsia.epizy.com/phprc2/exercicio2.php">ex2</a>
+      <a href="http://rcexerciciosgpsia.epizy.com/phprc2/exercicio3.php">ex3</a>
+        
+        
+    </div>
+  </div>
+    
+    <div class="dropdown">
+    <button class="dropbtn">RC3
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="http://rcexerciciosgpsia.epizy.com/phprc3/exercicio1.php">ex1</a>
+      <a href="http://rcexerciciosgpsia.epizy.com/phprc3/exercicio2.php">ex2</a>
+      <a href="http://rcexerciciosgpsia.epizy.com/phprc3/exercicio3.php">ex3</a>
+      <a href="http://rcexerciciosgpsia.epizy.com/phprc3/exercicio4.php">ex4</a>
+        
+        
+    </div>
+  </div>
+    
+    <div class="dropdown">
+    <button class="dropbtn">RC4
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="http://rcexerciciosgpsia.epizy.com/rc4/captar_get.php">captar_get</a>
+      <a href="http://rcexerciciosgpsia.epizy.com/rc4/captar_post.php">captar_post</a>
+      <a href="http://rcexerciciosgpsia.epizy.com/rc4/f_insc_get.php">f_insc_get</a>
+      <a href="http://rcexerciciosgpsia.epizy.com/rc4/f_insc_post.php">f_insc_post</a>
+        
+        
+    </div>
+  </div>
+    
+    <div class="dropdown">
+    <button class="dropbtn">RC5
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="http://rcexerciciosgpsia.epizy.com/rc5/entrar.php">entrar</a>
+      <a href="http://rcexerciciosgpsia.epizy.com/rc5/erro.php">erro</a>
+      <a href="http://rcexerciciosgpsia.epizy.com/rc5/login.php">login</a>
+      <a href="http://rcexerciciosgpsia.epizy.com/rc5/main.php">main</a>
+        
+        
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+</html>
